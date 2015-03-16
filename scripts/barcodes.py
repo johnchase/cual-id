9763cde321c72827faadc055acf7650f77433247
johnjohnchase <chasejohnh@gmail.com>
2015-03-16 10:59:46 -0700

    updated variable names

--- scripts/barcodes.py
+++ scripts/barcodes.py
@@ -6,12 +6,12 @@ from barcode.generate_barcode_pdfs import get_barcodes
 @click.option('--sep', type=str, default='tsv',
               help="Pass 'tsv' if file separator is tab-separator, "
                    "pass 'csv' if file is comma separated (default=tsv)")
-@click.option('-m', '--mapping', type=str, multiple=True,
+@click.option('-c', '--comment', type=str, multiple=True,
               help='Pass if comment lines should be ignored')
 @click.argument('input', type=click.File('U'), required=False)
 @click.argument('output', type=click.File('w'), required=False)
 
-def cli(input, output, mapping, sep):
+def cli(input, output, comment, sep):
     """This scripts generates a pdf of barcodes"""
     get_barcodes(input, output, mapping)
 

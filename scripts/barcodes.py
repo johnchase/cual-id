798a9afa73f95a5aff50499dc5c535c45df8bf28
johnjohnchase <chasejohnh@gmail.com>
2015-03-16 10:54:05 -0700

    added separator argument

--- scripts/barcodes.py
+++ scripts/barcodes.py
@@ -3,13 +3,17 @@ import click
 from barcode.generate_barcode_pdfs import get_barcodes
 
 @click.command()
+@click.option('--sep', type=str, default='tsv',
+              help="Pass 'tsv' if file separator is tab-separator, "
+                   "pass 'csv' if file is comma separated (default=tsv)")
 @click.option('-m', '--mapping', type=str, multiple=True,
               help='Pass if comment lines should be ignored')
 @click.argument('input', type=click.File('U'), required=False)
 @click.argument('output', type=click.File('w'), required=False)
-def cli(input, output, mapping):
+
+def cli(input, output, mapping, sep):
     """This scripts generates a pdf of barcodes"""
-    get_barcodes(input, output, mapping, columns=4, rows=9)
+    get_barcodes(input, output, mapping)
 
 if __name__ == '__main__':
     main()

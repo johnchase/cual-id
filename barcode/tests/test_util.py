6999657e9f305779dde84c7b6f356db900960a01
johnjohnchase <chasejohnh@gmail.com>
2015-03-17 14:52:18 -0700

    updated tests

--- barcode/tests/test_util.py
+++ barcode/tests/test_util.py
@@ -8,6 +8,7 @@ from barcode.util import get_ids
 class GetIds(unittest.TestCase):
     def setUp(self):
         self.id_file = StringIO(id_file)
+        self.id_file2 = StringIO(id_file2)
 
     def test_get_ids(self):
         sep = '\t'
@@ -17,7 +18,20 @@ class GetIds(unittest.TestCase):
         exp = pd.Series(['Test_ID1', 'Test_ID2'])
         self.assertEqual(list(obs), list(exp))
 
+    def test_get_ids_with_comment(self):
+        sep = '\t'
+        comment = '#'
+        column = None
+        obs = get_ids(self.id_file2, comment, sep, column)
+        exp = pd.Series(['Test_ID1', 'Test_ID2'])
+        self.assertEqual(list(obs), list(exp))
+
 id_file = """#SampleID
+Test_ID1
+Test_ID2"""
+
+id_file2 = """#SampleID
+##comment line
 Test_ID1
 Test_ID2"""
 

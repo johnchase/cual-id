18bceb538d69e9955019308ef17c44d812985005
johnjohnchase <chasejohnh@gmail.com>
2015-03-17 18:23:19 -0700

    changed setup file

--- setup.py
+++ setup.py
@@ -1,24 +1,23 @@
-from setuptools import setup, find_packages
+#!/usr/bin/env python
+# copied and modified from https://github.com/biocore/scikit-bio
+from setuptools import find_packages, setup
 from glob import glob
+__version__ = "0.0.1-dev"
 
 
+description = ('Make a pdf out of barcodes')
 
-setup(
-    name='barcodes',
-    version='0.0',
-    py_modules=['barcodes'],
-    install_requires=[
-        'Click',
-        'reportlab',
-        'pandas'
-    ],
-    packages=find_packages(),
-    scripts=glob("scripts/*"),
-    entry_points='''
-        [console_scripts]
-        BC-generator=scripts.barcodes:cli
-    ''',
-    test_suite='nose.collector',
-    extras_require={'test': ["nose >= 0.10.1", "pep8", "flake8"]},
+with open('README.md') as f:
+    long_description = f.read()
 
-)
+setup(name='barcodes',
+      version=__version__,
+      description=description,
+      long_description=long_description,
+      test_suite='nose.collector',
+      packages=find_packages(),
+      scripts=glob("scripts/*"),
+      install_requires=['pandas', 'click'],
+      extras_require={'test': ["nose >= 0.10.1", "pep8", "flake8"],
+                      'doc': ["Sphinx"]},
+      )

229b4a8eb90d5eefa3ce58fddad62d246274f230
johnjohnchase <chasejohnh@gmail.com>
2015-03-17 18:00:45 -0700

    added a change

--- scripts/barcodes.py
+++ scripts/barcodes.py
@@ -4,11 +4,12 @@ from barcode.util import get_barcodes
 
 
 @click.command()
-@click.argument('input', type=click.File('U'), required=False)
+@click.argument('input', type=click.File('U'), required=True)
 @click.argument('output', type=click.File('w'), required=False)
 def cli(input, output):
     """This scripts generates a pdf of barcodes"""
+    print input
     get_barcodes(input, output)
 
 if __name__ == '__main__':
-    main()
+    cli()

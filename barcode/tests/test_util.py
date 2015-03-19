6c6fd9b5afe290fbe3bf1de86ebcab8dac023a47
johnjohnchase <chasejohnh@gmail.com>
2015-03-19 10:20:26 -0700

    added tests

--- barcode/tests/test_util.py
+++ barcode/tests/test_util.py
@@ -1,7 +1,7 @@
 import unittest
 from StringIO import StringIO
 
-from barcode.util import get_ids
+from barcode.util import get_ids, get_x_y_coordinates
 
 
 class GetIds(unittest.TestCase):
@@ -12,6 +12,18 @@ class GetIds(unittest.TestCase):
 
         obs = get_ids(self.id_file)
         exp = ['Test_ID1', 'Test_ID2']
+        self.assertEqual(obs, exp)
+
+    def test_get_x_y_coordinates(self):
+        columns = 2
+        rows = 2
+        x_start = 0
+        y_start = 10
+        obs = get_x_y_coordinates(columns, rows, x_start, y_start)
+        exp = [(0.0, 720.0),
+               (0.0, 632.87999999999988),
+               (158.40000000000001, 720.0),
+               (158.40000000000001, 632.87999999999988)]
         self.assertEqual(obs, exp)
 
 id_file = """#SampleID

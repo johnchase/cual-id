e7c69d4e246ae42b77cdb6f948e69b88f9836da0
ebolyen <ebolyen@gmail.com>
2015-09-30 14:53:52 -0700

    fix-ids refactored to support streaming and tests

--- cualid/fix.py
+++ cualid/fix.py
@@ -1,48 +1,38 @@
 from difflib import get_close_matches
 
 
-def fix_ids(correct_input, input_to_check, show, all, thresh=.5):
-    if all:
-        show = 'DFNV'
-
+def fix_ids(correct_input, input_to_check, thresh=.5):
     corr_ids = [e.strip().split('\t')[1] for e in correct_input]
 
     broke_ids = [e.strip().split('\t')[0] for e in input_to_check]
 
-    fixed_ids = []
+    seen = set()
     for broke_id in broke_ids:
         fixed_id = get_close_matches(broke_id, corr_ids, 1, thresh)
         if not fixed_id:
             fixed_id = ''
         else:
             fixed_id = fixed_id[0]
-        fixed_ids.append(fixed_id)
 
-    err_codes = get_err_codes(broke_ids, fixed_ids)
-    return format_ouput(broke_ids, fixed_ids, err_codes, show)
+        err_code = get_err_code(broke_id, fixed_id, seen)
+        seen.add(fixed_id)
+        yield broke_id, fixed_id, err_code
 
 
-def format_ouput(broke_ids, fixed_ids, err_codes, show):
-    lines = []
+def format_output(output, show):
     show = list(show)
-    for line in zip(broke_ids, fixed_ids, err_codes):
+    for line in output:
         if any(i in list(line[2]) for i in show):
-            lines.append('\t'.join(line))
-        else:
-            pass
-    return '\n'.join(lines)
+            yield '\t'.join(line)
 
 
-def get_err_codes(broke_ids, fixed_ids, ):
-    err_codes = []
-    for broke_id, fixed_id in zip(broke_ids, fixed_ids):
-        if fixed_id == '':
-            err_code = 'N'
-        elif broke_id == fixed_id:
-            err_code = 'V'
-        else:
-            err_code = 'F'
-        if fixed_ids.count(fixed_id) > 1 and fixed_id != '':
-            err_code = 'D' + err_code
-        err_codes.append(err_code)
-    return err_codes
+def get_err_code(broke_id, fixed_id, seen):
+    if fixed_id == '':
+        err_code = 'N'
+    elif broke_id == fixed_id:
+        err_code = 'V'
+    else:
+        err_code = 'F'
+    if fixed_id != '' and fixed_id in seen:
+        err_code = 'D' + err_code
+    return err_code

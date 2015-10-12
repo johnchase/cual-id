5ac6120e97c51df176f598f4c38244674c93802d
ebolyen <ebolyen@gmail.com>
2015-10-12 15:37:41 -0700

    update setup

--- setup.py
+++ setup.py
@@ -2,15 +2,35 @@
 # copied and modified from https://github.com/biocore/scikit-bio
 from setuptools import find_packages, setup
 from glob import glob
-__version__ = "0.0.1-dev"
+__version__ = "0.9.0"
 
+classes = """
+    Development Status :: 4 - Beta
+    License :: OSI Approved :: BSD License
+    Intended Audience :: Science/Research
+    Topic :: Utilities
+    Topic :: Scientific/Engineering
+    Programming Language :: Python :: 3
+    Programming Language :: Python :: 3.3
+    Programming Language :: Python :: 3.4
+    Operating System :: Unix
+    Operating System :: POSIX
+    Operating System :: MacOS :: MacOS X
+    Operating System :: Microsoft :: Windows
+"""
+classifiers = [s.strip() for s in classes.split('\n') if s]
 
-description = ('Make a pdf out of barcodes')
+description = ('A package for creating and managing sample identifiers in'
+               ' comparative -omics datasets.')
 
-with open('README.md') as f:
+with open('README.rst') as f:
     long_description = f.read()
 
-setup(name='Cual-ID',
+setup(name='cual-id',
+      author="John Chase",
+      author_email="chasejohnh@gmail.com",
+      url="https://github.com/johnchase/cual-id",
+      classifiers=classifiers,
       version=__version__,
       description=description,
       long_description=long_description,
